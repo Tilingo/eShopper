11bbef3f34c966e614a9c5af153c5b9e978f63da
Tilingo <nestirco@gmail.com>
2018-06-25 14:54:38 -0400

    Both apps running on port 3000 and 3001

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
+require("dotenv").config();
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
 
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+let usersRouter = require('./routes/users')
+let storesRouter = require('./routes/stores')
+let productsRouter = require('./routes/products')
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static(__dirname + '/client/build/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
+app.use('/api/users', usersRouter)
+// app.use('/api/users/userId/stores', storesRouter)
+// app.use('/api/users/userId/stores/storeId/products', productsRouter)
 
 module.exports = app;
