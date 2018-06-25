eba7e6838bfdde1430b48e1dab35b93bbdefa9bf
Tilingo <nestirco@gmail.com>
2018-06-25 19:56:24 -0400

    Store's CREATE route created

--- routes/stores.js
+++ routes/stores.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router({mergeParams: true})
 const User = require('../models/User')
-// const Store = require('../models/Store')
+const Store = require('../models/Store')
 
 router.get('/', async (req, res) =>{
     const user = await User.findById(req.params.userId)
     res.send({
         stores: user.stores
     })
+})
+
+router.post('/', (req, res) => {
+    const newStore = new Store(req.body)
+    
+    User.findById(req.params.userId)
+    .then(user=>{
+        user.stores.push(newStore)
+        return user.save()
+    })
+    .then(()=>{
+        res.send({
+            newStore
+        })
+    })
+
 })
 
 module.exports = router
