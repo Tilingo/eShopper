76500b831face847dc8f2a9d28bc96e5020c9f19
Tilingo <nestirco@gmail.com>
2018-06-26 09:59:20 -0400

    User's form created and tested

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+
+class SignUp extends Component {
+
+    state={
+        e_mail: '',
+        userName: '',
+        password: ''
+    }
+
+    handleChange = (event) =>{
+        const userInput = event.target.value
+        const inputName = event.target.name
+
+        this.setState({
+            [inputName]: userInput
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/api/users', this.state).then(res=>{
+            console.log(res)
+            this.props.history.push(`/users/${res.data.user._id}`)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} type="email" name="e_mail" placeholder="e-mail address"/>
+                    <input onChange={this.handleChange} type="text" name="userName" placeholder="User Name"/>
+                    <input onChange={this.handleChange} type="password" name="password" placeholder="Password"/>
+                    <button type="submit">CREATE ACCOUNT</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
