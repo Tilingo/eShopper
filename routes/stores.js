db754ba4043b624220442f252ab35588acbedb91
Tilingo <nestirco@gmail.com>
2018-06-25 20:35:18 -0400

    Store's UPDATE route created

--- routes/stores.js
+++ routes/stores.js
@@ -35,4 +35,21 @@ router.get('/:id', (req, res)=>{
     })
 })
 
+router.patch('/:id', (req, res)=>{
+    User.findById(req.params.userId)
+    .then(user=>{
+        const store = user.stores.id(req.params.id)
+
+        store.name = req.body.name
+        store.description = req.body.description
+
+        return user.save()
+    })
+    .then((user)=>{
+        res.send({
+            store: user.stores.id(req.params.id)
+        })
+    })
+})
+
 module.exports = router
